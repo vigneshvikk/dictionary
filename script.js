454dbfae2afa421742a824deb6e0c15229c8d2fc
unknown <vigneshvikky199923@gmail.com>
2023-03-07 20:19:54 +0530

    second

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function searchWord(){
+
     word1=word.value
 
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word1}`)
     .then(data => data.json()).then(data=>display(data))
 
-   
+
 }
+
+
+
+
 
 function display(WordData){
 
     meaning=WordData[0].meanings[0].definitions[0].definition
     word2=WordData[0].word
-    // aud=WordData[0].phonetics[0].audio
-
+    aud=WordData[0].phonetics[0].audio
 
     result.innerHTML=`
+    <audio src="${aud}" controls   class="mt-2">
     
+</audio>
     <h1><srong> ${word2} </srong> </h1>
     <p> ${meaning} </p>
     `
 }
+
+
+
 
